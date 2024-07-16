f8351fd07c455114d0c8d1070e21e3cf921303a3
Kieran Llarena <baymax709@gmail.com>
2024-07-16 12:32:42 -0400

    feat: simulate fetching data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rand::{rngs::ThreadRng, Rng};
+
+fn simulate_fetching_data<'a>() -> Result<&'a str, &'a str> {
+    let mut rng: ThreadRng = rand::thread_rng();
+    let val: i32 = rng.gen_range(0..=1);
+
+    if val == 0 {
+        Ok("Data was fetched successfully!")
+    } else {
+        Err("There was an error fetching the data")
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
